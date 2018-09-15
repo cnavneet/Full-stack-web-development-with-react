0e1391bfca85cf83ac695cd4358c3f77fad20887
cnavneet <cnavneet58@gmail.com>
2018-09-15 19:59:46 +0530

    Assignment 3 successfully completed

--- react/confusion/src/components/DishDetailComponent.js
+++ react/confusion/src/components/DishDetailComponent.js
@@ -1,6 +1,99 @@
-import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalBody, ModalHeader, Label, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
+const maxLength = (len) => (val) => !val || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+
+class CommentForm extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            isModalOpen: false
+        }
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        })
+    }
+
+    handleSubmit(values) {
+        this.toggleModal();
+        alert("Current state is: " + JSON.stringify(values));
+    }
+
+    render(){
+        return(
+            <div className = "container">
+                <Button outline onClick = {this.toggleModal}>
+                    <span className = "fa fa-edit fa-lg"></span> Add Comments
+                </Button>
+                <Modal isOpen = {this.state.isModalOpen} toggle = {this.toggleModal}>
+                    <ModalHeader toggle = {this.toggleModal}>
+                        Add Comments
+                    </ModalHeader>
+                    <ModalBody>
+                        <div className = "col-12 col-md-9">
+                            <LocalForm onSubmit = {(values) => this.handleSubmit(values)}>
+                                <Row className = "form-group">
+                                    <Label htmlFor = "rating" md={2}>Rating</Label>
+                                    <Col md = {12}>
+                                        <Control.select model = ".rating" name = "rating" className = "form-control">
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </Control.select>
+                                    </Col>
+                                </Row>
+                                <Row className = "form-group">
+                                    <Label htmlFor = "author" md = {12}>Your Name</Label>
+                                    <Col md = {12}>
+                                        <Control.text model = ".author" id = "author" name = "author" className = "form-control"
+                                        validators = {{
+                                            minLength: minLength(3), maxLength: maxLength(15)
+                                        }}/>
+                                        <Errors
+                                            className="text-danger"
+                                            model=".author"
+                                            show="touched"
+                                            messages={{
+                                                minLength: 'Must be greater than 2 characters',
+                                                maxLength: 'Must be 15 characters or less'
+                                            }}
+                                        />
+                                    </Col>
+                                </Row>
+                                <Row className = "form-group">
+                                    <Label htmlFor = "comment" md = {12}>Comment</Label>
+                                    <Col md = {12}>
+                                        <Control.textarea model = ".comment" id = "comment" name = "comment"
+                                        rows = "6"
+                                        className = "form-control" />
+                                    </Col>
+                                </Row>
+                                <Row className = "form-group">
+                                    <Col md = {{size:10, offset: 2}}>
+                                        <Button type = "submit" color = "primary">
+                                            Submit
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </LocalForm>
+                        </div>
+                    </ModalBody>
+                </Modal>
+            </div>
+        )
+    }
+}
 
 function RenderDish({dish}){
     return(
@@ -25,13 +118,14 @@ function RenderComments({comments}){
                 <ul className = "list-unstyled">
                     {comments.map((comment) => {
                         return(
-                            <li key = {comment.id}>
-                                <p>{comment.comment}</p>
-                                <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                            </li>
+                                <li key = {comment.id}>
+                                    <p>{comment.comment}</p>
+                                    <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                </li>
                         );
                     })}
                 </ul>
+                <CommentForm />
             </div>
         );
     }
