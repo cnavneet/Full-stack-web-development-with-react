5a00b3f356ddcc5dd05aff0b8c8297ca3dcade4a
cnavneet <cnavneet58@gmail.com>
2018-09-17 16:21:19 +0530

    Fetch Post Comments

--- react/confusion/src/components/DishDetailComponent.js
+++ react/confusion/src/components/DishDetailComponent.js
@@ -27,7 +27,7 @@ class CommentForm extends Component{
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render(){
@@ -111,7 +111,7 @@ function RenderDish({dish}){
     );
 }
 
-function RenderComments({comments, addComment, dishId}){
+function RenderComments({comments, postComment, dishId}){
     if(comments != null)
     {
         return(
@@ -127,7 +127,7 @@ function RenderComments({comments, addComment, dishId}){
                         );
                     })}
                 </ul>
-                <CommentForm dishId = {dishId} addComment = {addComment} />
+                <CommentForm dishId = {dishId} postComment = {postComment} />
             </div>
         );
     }
@@ -169,7 +169,7 @@ function RenderComments({comments, addComment, dishId}){
                     <div className = "row">
                         <RenderDish dish = {props.dish} />
                         <RenderComments comments = {props.comments}
-                            addComment={props.addComment}
+                            postComment={props.postComment}
                             dishId = {props.dish.id} />
                     </div>
                 </div>
--- react/confusion/src/components/MainComponent.js
+++ react/confusion/src/components/MainComponent.js
@@ -8,7 +8,7 @@ import About from './AboutComponent';
 import Footer from './FooterComponent';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import { connect } from 'react-redux';
-import {addComment, fetchDishes, fetchComments, fetchPromos} from '../redux/ActionCreators';
+import {addComment, fetchDishes, fetchComments, fetchPromos, postComment} from '../redux/ActionCreators';
 import {actions} from 'react-redux-form';
 
 const mapStateToProps = state => {
@@ -25,7 +25,8 @@ const mapDispatchToProps = (dispatch) => ({
   fetchDishes: () => { dispatch(fetchDishes())},
   resetFeedbackForm: () => {dispatch(actions.reset('feedback'))},
   fetchComments: () => { dispatch(fetchComments())},
-  fetchPromos: () => { dispatch(fetchPromos())}
+  fetchPromos: () => { dispatch(fetchPromos())},
+  postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment))
 });
 
 class Main extends Component {
@@ -62,7 +63,7 @@ class Main extends Component {
             errMess={this.props.dishes.errMess}
             comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
             commentsErrMess={this.props.comments.errMess}
-            addComment={this.props.addComment}
+            postComment={this.props.postComment}
           />
         );
     }
