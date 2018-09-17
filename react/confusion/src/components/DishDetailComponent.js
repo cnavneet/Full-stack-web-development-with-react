75dfdbf88955327060300098f6a493c668fef2ca
cnavneet <cnavneet58@gmail.com>
2018-09-17 15:24:33 +0530

    fetch from server

--- react/confusion/src/components/DishDetailComponent.js
+++ react/confusion/src/components/DishDetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrum
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
 
 const maxLength = (len) => (val) => !val || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -100,7 +101,7 @@ function RenderDish({dish}){
     return(
         <div className = "col-12 col-md-5 m-1">
             <Card>
-                <CardImg top src = {dish.image} alt = {dish.name}/>
+                <CardImg top src = {baseUrl + dish.image} alt = {dish.name}/>
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
