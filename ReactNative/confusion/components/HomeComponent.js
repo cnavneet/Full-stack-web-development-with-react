11c0b249eba694741088a4709d53766a2ef5f1e6
cnavneet <cnavneet58@gmail.com>
2018-10-08 18:04:37 +0530

    React Native: navigation 2

---
+++ ReactNative/confusion/components/HomeComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Home extends Component {
+    static navigationOptions = {
+        title: 'Home'
+    };
+    render() {
+        return(
+            <View><Text>Home Component</Text></View> 
+        );
+    }
+}
+
+export default Home;
--- ReactNative/confusion/components/MainComponent.js
+++ ReactNative/confusion/components/MainComponent.js
@@ -3,7 +3,9 @@ import Dishdetail from './DishDetailComponent';
 import { View, Platform } from 'react-native';
 import Menu from './MenuComponent';
 import {DISHES} from '../shared/dishes';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import Home from './HomeComponent';
+import {Icon} from 'react-native-elements';
 
 const MenuNavigator = createStackNavigator({
     Menu: { screen: Menu },
@@ -23,6 +25,39 @@ const MenuNavigator = createStackNavigator({
 }
 );
 
+const HomeNavigator = createStackNavigator({
+    Home: { screen: Home }
+  }, {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+          color: "#fff"            
+      },
+      headerTintColor: "#fff"  
+    })
+});
+
+const MainNavigator = createDrawerNavigator({
+    Home: 
+      { screen: HomeNavigator,
+        navigationOptions: {
+          title: 'Home',
+          drawerLabel: 'Home'
+        }
+      },
+    Menu: 
+      { screen: MenuNavigator,
+        navigationOptions: {
+          title: 'Menu',
+          drawerLabel: 'Menu'
+        }, 
+      }
+}, {
+  drawerBackgroundColor: '#D1C4E9'
+});
+
 class Main extends Component {
     constructor(props){
         super(props);
@@ -39,7 +74,7 @@ class Main extends Component {
     render() {
         return (
             <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-                <MenuNavigator />
+                <MainNavigator />
             </View>
         );
     }
