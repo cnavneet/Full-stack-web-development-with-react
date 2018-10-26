f63d4f343af68a86e8fb297312c1100a588be8a3
cnavneet <cnavneet58@gmail.com>
2018-10-26 06:20:44 +0530

    React Native: Assignment 1 successfully completed

--- ReactNative/confusion/components/MainComponent.js
+++ ReactNative/confusion/components/MainComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Dishdetail from './DishDetailComponent';
 import { View, Platform } from 'react-native';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
 import {DISHES} from '../shared/dishes';
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 import Home from './HomeComponent';
@@ -39,6 +41,34 @@ const HomeNavigator = createStackNavigator({
     })
 });
 
+const ContactNavigator = createStackNavigator({
+    Contact: { screen: Contact}
+    }, {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTitleStyle: {
+                color: "#fff"
+            },
+            headerTintColor: "#fff"
+        }
+    });
+
+    const AboutNavigator = createStackNavigator({
+        About: { screen: About}
+        }, {
+            navigationOptions: {
+                headerStyle: {
+                    backgroundColor: "#512DA8"
+                },
+                headerTitleStyle: {
+                    color: "#fff"
+                },
+                headerTintColor: "#fff"
+            }
+        });
+
 const MainNavigator = createDrawerNavigator({
     Home: 
       { screen: HomeNavigator,
@@ -47,12 +77,28 @@ const MainNavigator = createDrawerNavigator({
           drawerLabel: 'Home'
         }
       },
+      About:
+      {
+          screen: AboutNavigator,
+          navigationOptions: {
+              title: 'About',
+              drawerLabel: 'About Us'
+          }
+      },
     Menu: 
       { screen: MenuNavigator,
         navigationOptions: {
           title: 'Menu',
           drawerLabel: 'Menu'
         }, 
+      },
+    Contact:
+      {
+          screen: ContactNavigator,
+          navigationOptions: {
+              title: 'Contact Us',
+              drawerLabel: 'Contact Us'
+          }
       }
 }, {
   drawerBackgroundColor: '#D1C4E9'
