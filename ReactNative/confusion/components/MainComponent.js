0a8aea145ae4178279af58eb4a81cc5b54d07dbd
navneet <cnavneet58@gmail.com>
2018-11-09 23:55:53 +0530

    React Native: Picking an image

--- ReactNative/confusion/components/MainComponent.js
+++ ReactNative/confusion/components/MainComponent.js
@@ -142,22 +142,23 @@ const ReservationNavigator = createStackNavigator({
             })
         });
 
-    const LoginNavigator = createStackNavigator({
-        Login: { screen: Login }
-        }, {
-        navigationOptions: ({ navigation }) => ({
-        headerStyle: {
-            backgroundColor: "#512DA8"
-        },
-        headerTitleStyle: {
-            color: "#fff"            
-        },
-        headerTintColor: "#fff",
-        headerLeft: <Icon name="menu" size={24}
-            iconStyle={{ color: 'white' }} 
-            onPress={ () => navigation.toggleDrawer() } />    
-        })
-    });
+        const LoginNavigator = createStackNavigator({
+            Login: Login
+          }, {
+          navigationOptions: ({ navigation }) => ({
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTitleStyle: {
+                color: "#fff"            
+            },
+            title: 'Login',
+            headerTintColor: "#fff",
+            headerLeft: <Icon name="menu" size={24}
+              iconStyle={{ color: 'white' }} 
+              onPress={ () => navigation.toggleDrawer() } />    
+          })
+        });
 
 const CustomDrawerContentComponent = (props) => (
     <ScrollView>
