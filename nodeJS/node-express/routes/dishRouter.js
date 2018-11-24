e57aa0d905297495a500473d2694a14c36e73cf5
navneet <cnavneet58@gmail.com>
2018-11-24 20:37:49 +0530

    nodeJS: Assignment 1 successfully completed

--- nodeJS/node-express/routes/dishRouter.js
+++ nodeJS/node-express/routes/dishRouter.js
@@ -29,4 +29,29 @@ dishRouter.route('/')
     res.end('Deleting all dishes');
 });
 
+dishRouter.route('/:dishId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req, res, next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+})
+
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/' + req.params.dishId);
+})
+
+.put((req, res, next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+})
+
+.delete((req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+})
+
 module.exports = dishRouter;
