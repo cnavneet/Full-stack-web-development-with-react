a9ecffb8116851b22574492362a664efc39df9cf
navneet <cnavneet58@gmail.com>
2018-11-24 19:06:12 +0530

    nodeJS: Express serve static files

--- nodeJS/node-express/index.js
+++ nodeJS/node-express/index.js
@@ -1,13 +1,16 @@
 const express = require('express'),
         http = require('http');
+const morgan = require('morgan');
 
 const hostname = "localhost";
 const port = "3000";
 
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is express server</h1></body></html>');
